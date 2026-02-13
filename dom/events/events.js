a5f79c98f304b6801dc4fe54d4c4ee31f6fc971a
ramprasathmk <mkramprasath12@gmail.com>
2026-02-13 15:52:45 +0530

    dom events

---
+++ dom/events/events.js
@@ -0,0 +1,14 @@
+"Mikhail Tal";
+
+// let counter = 0;
+// // Here, it is button object
+// let myButton = document.querySelectorAll("button")[0];
+
+// let myH1 = document.querySelectorAll("h1")[0];
+
+// myButton.textContent = "Click Here!";
+
+// myButton.addEventListener("click", () => {
+//     console.log("Im from click event.");
+//     myH1.innerText = ++counter;
+// });
